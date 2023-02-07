870e40e2a9ae8384103d5f47b4dfd85b4dbc2a43
Albert <benjaminalbert165812@gmail.com>
2023-02-07 09:13:23 +0700

    change app.run

--- server.py
+++ server.py
@@ -93,6 +93,6 @@ def data_lesson():
 
 # Start Backend
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=6868)
-    # app.run(host="0.0.0.0", port=6868)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=6868)
+    app.run(host="0.0.0.0", port=6868)
